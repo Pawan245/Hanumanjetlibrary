1edd40bb92992b9e58054ae4b01afb63e26ff382
Pawan Kumar <55477922+Pawan245@users.noreply.github.com>
2021-07-28 23:19:45 +0530

    update

--- hanumanjetpacklibrery/src/main/java/com/ram/hanumanjetpacklibrery/StartAnyActivity.java
+++ hanumanjetpacklibrery/src/main/java/com/ram/hanumanjetpacklibrery/StartAnyActivity.java
@@ -5,12 +5,21 @@ import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StartAnyActivity extends AppCompatActivity {
+public  class StartAnyActivity<T> extends AppCompatActivity {
 
-public void StartActivityAny(Context packageContext, Class<?> cls) {
+public void startActivityAnyAndFinish(Context packageContext, Class<T> cls) {
     Intent intent = new Intent(packageContext,  cls);
     startActivity(intent);
+    finish();
 
 }
+
+    public void startActivityAny(Context packageContext, Class<T> cls) {
+        Intent intent = new Intent(packageContext,  cls);
+        startActivity(intent);
+    }
+
+
+
 
 }
